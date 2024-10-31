993672612889b9a2d1778141d352b8b909cd83c3
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2024-10-31 12:15:04 +0200

    set up client bundle

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>I am the Home components</div>
+  return <div>
+    <div>I am the Super BEST Home components</div>
+    <button onClick={() => console.log('Hi')}>Press me</button>
+  </div>
+
 }
 
 export default Home;
