ec7cbe51b8563eb59a43ffc1fb492ae9aa085bd5
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2025-01-09 14:21:13 +0200

    Header customization

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import {connect} from "react-redux";
 
-const Header = () => {
+const Header = ({auth}) => {
+
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+
   return (
-    <div>
-      <Link to="/">React SSR</Link>
+    <nav>
+    <div className="nav-wrapper">
+      <Link to="/" className="brand-logo">React SSR</Link>
+      <ul className="right">
+        <li><Link to="/users">Users</Link></li>
+        <li><Link to="/admins">Admins</Link></li>
+        <li>{authButton}</li>
+      </ul>
     </div>
+    </nav>
   );
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+  return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Home = () => {
-  return <div>
-    <div>I am the Super BEST Home components</div>
-    <button onClick={() => console.log('Hi')}>Press me</button>
+  return <div className="center-align" style={{marginTop: '200px'}}>
+    <h3>Welcome</h3>
+    <p>Check out these awesome features</p>
   </div>
 
 }
